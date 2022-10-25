8f2863f78032d82f2dd2e35a4b003893b3733430
Victor Ferrer <victorfergar@gmail.com>
2022-10-25 20:22:36 +0200

    feat: Migrate Github set-output commands to environment files
    https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands

--- .github/actions/get-change-set-report/entrypoint.sh
+++ .github/actions/get-change-set-report/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo ::set-output name=html_description::$(echo $INPUT_JSON_DESCRIPTION | python /format-change-set-json-as-html.py)
+echo "html_description=$(echo $INPUT_JSON_DESCRIPTION | python /format-change-set-json-as-html.py)" >> $GITHUB_OUTPUT
